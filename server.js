0cf73f21456fc42588f862080252edef0dafe956
mdfaizan42500 <mdfaizan42500@gmail.com>
2026-06-21 14:45:50 +0530

    smtp

--- server.js
+++ server.js
@@ -27,12 +27,14 @@ app.post("/send-email", async (req, res) => {
     const { name } = req.body;
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
     const info = await transporter.sendMail({
       from: process.env.EMAIL_USER,
